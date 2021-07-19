0840fd7cbf40826fad06f571e48c5bd655fbee00
lorweiuk <lweise@ukaachen.de>
2021-07-19 14:54:51 +0200

    initial commit

---
+++ pig_hole_game.R
@@ -0,0 +1,109 @@
+
+pig_hole_game <- function(n_max_rounds, n_marbles_each, player_begin, strategy_player, strategy_enemy) {
+    
+  # dice throws for all rounds
+  rnd_dice <- round(runif(n = n_max_rounds, min = 0.5000001, max = 6.4999999))
+  
+  # simulate all rounds
+  board_state <- matrix(0, nrow = n_max_rounds, ncol = 6)
+  
+  # marble count for player (col 1) and opponent (col 2)
+  n_marbles <- matrix(NaN, nrow = n_max_rounds, ncol = 2)
+  n_marbles[1, ] = n_marbles_each
+  
+  # record of who's turn it is
+  players_turn <- rep(NaN, times = n_max_rounds)
+  
+  players_round <- if (player_begin) { 1 } else { 2 } # who's turn is it? column indicator for n_marbles matrix
+  
+  for (nr in 1:n_max_rounds) {
+    
+    # register who's turn it is
+    players_turn[nr] = players_round
+    
+    # register dice roll
+    if (nr > 1)
+      board_state[nr, ] = board_state[nr - 1, ] # copy previous round's state
+
+    board_state[nr, rnd_dice[nr]] = board_state[nr, rnd_dice[nr]] + 1 # add marble according to dice
+    
+    # copy previous round's scores
+    if (nr > 1)
+      n_marbles[nr, ] = n_marbles[nr - 1, ]
+    
+    n_marbles[nr, players_round] = n_marbles[nr, players_round] - 1 # remove marble from player
+    
+    # check if too many marbles in field
+    if (any(board_state[nr, 1:5] > 1:5)) {
+      # add content of board to current player's marble count; subtract one for the currently moved stone
+      n_marbles[nr, players_round] = n_marbles[nr, players_round] + sum(board_state[nr, 1:5])
+      
+      board_state[nr, 1:5] = 0 # reset board
+      
+      players_round = 3 - players_round # change player
+      
+    } else if (nr <= 6) {
+      # if game is still in the first 6 rounds ... don't allow strategy
+      
+      if (nr == 1 || nr == 2 || nr == 4 || nr == 6) {
+        # on rounds 1, 2, 4 and 6, force player change
+        players_round = 3 - players_round
+      }
+      
+    } else {
+      # after 6 rounds, and if no field is overfilled: allow strategy
+      decision <- NULL
+      
+      if (players_round == 1) {
+        decision <- strategy_player(board_state[nr, ], n_marbles[nr, 1], n_marbles[nr, 2])
+        
+      } else if (players_round == 2) {
+        decision <- strategy_enemy(board_state[nr, ], n_marbles[nr, 2], n_marbles[nr, 1])
+      }
+      
+      if (decision)
+        players_round = 3 - players_round # change player
+    }
+    
+    # check if game is over
+    if (any(n_marbles[nr, ] == 0))
+      break
+  }
+  
+  # return results
+  output <- cbind(rnd_dice, players_turn, board_state, n_marbles)
+  
+  colnames(output) = c("dice", "turn", "board_1", "board_2", "board_3", "board_4", "board_5", "board_6", "marbles_player", "marbles_enemy")
+  
+  return(output)
+}
+
+############
+# simulate #
+############
+
+n_max_rounds <- 300
+
+n_marbles_each <- 30
+
+player_begin <- TRUE
+
+strategy_2fields <- function(board, player_marbles, enemy_marbles) {
+  # function defining player's strategy
+  if (sum(board[1:5] == c(1, 2, 3, 4, 5)) >= 2) {
+    return(1)
+  } else {
+    return(0)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
